976a582abc7e3e1d3bf007da6fd0e3e5d7de6a2c
Julian Metzler <git@mezgr.de>
2023-03-05 17:03:09 +0100

    Add label generation and font

--- tools.py
+++ tools.py
@@ -1,7 +1,9 @@
 import argparse
+import code128
 import csv
 import time
 
+from PIL import Image, ImageDraw, ImageFont
 from pprint import pprint
 
 from secrets import *
@@ -11,12 +13,18 @@ from partkeepr import PartKeepr
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--action", type=str, required=True, choices=('sync-distributors', 'list-empty-part-mf', 'update-locations-from-csv'), help="Which action to perform")
+    parser.add_argument("-a", "--action", type=str, required=True, choices=('sync-distributors', 'list-empty-part-mf', 'update-locations-from-csv', 'generate-labels'), help="Which action to perform")
     parser.add_argument("-f", "--force", action='store_true', help="Force certain actions")
     parser.add_argument("--name-column", type=str, required=False, help="For CSV import: Name column name")
     parser.add_argument("--location-column", type=str, required=False, help="For CSV import: Storage location column name")
     parser.add_argument("--default-location", type=str, required=False, help="For CSV import: Default storage location if none is found")
     parser.add_argument("--csv-file", type=str, required=False, help="For CSV import: CSV file name")
+    parser.add_argument("--label-width", type=int, required=False, help="For label generation: Label width in millimeters")
+    parser.add_argument("--label-height", type=int, required=False, help="For label generation: Label height in millimeters")
+    parser.add_argument("--label-dpi", type=int, required=False, help="For label generation: Label resolution in dpi")
+    parser.add_argument("--font-size", type=int, required=False, help="For label generation: Font size")
+    parser.add_argument("--max-parts-per-label", type=int, required=False, help="For label generation: Only generate label for maximum of n parts")
+    parser.add_argument("--label-file", type=str, required=False, help="For label generation: Label PDF file name")
     args = parser.parse_args()
     
     pk = PartKeepr(PK_BASE_URL, PK_USERNAME, PK_PASSWORD)
@@ -137,6 +145,7 @@ def main():
                                 part['parameters'].append({'name': param_name, 'stringValue': param_value})
                         result = pk.update_part(part)
             time.sleep(0.2) # To ensure we don't exceed 5 TME API calls per second
+    
     elif args.action == 'list-empty-part-mf':
         print("Getting parts")
         parts = pk.get_parts()
@@ -151,6 +160,7 @@ def main():
         
         print("Parts without part manufacturers:")
         print("\n".join(empty_mf_parts))
+    
     elif args.action == 'update-locations-from-csv':
         if not args.name_column or not args.location_column or not args.csv_file or not args.default_location:
             print("Error: Missing parameters!")
@@ -200,7 +210,78 @@ def main():
             print("  Updating part")
             part['storageLocation'] = {'@id': loc_id}
             result = pk.update_part(part)
+    
+    elif args.action == 'generate-labels':
+        if not args.label_width or not args.label_height or not args.label_dpi or not args.font_size or not args.max_parts_per_label or not args.label_file:
+            print("Error: Missing parameters!")
+            return
+        
+        label_width_px = round((args.label_width / 25.4) * args.label_dpi)
+        label_height_px = round((args.label_height / 25.4) * args.label_dpi)
+        
+        print("Getting parts")
+        parts = pk.get_parts()
+        parts_by_location = {}
+        
+        for part in parts:
+            if not part['storageLocation']:
+                continue
+            loc_name = part['storageLocation']['name']
+            if loc_name in parts_by_location:
+                parts_by_location[loc_name].append(part)
+            else:
+                parts_by_location[loc_name] = [part]
+        
+        labels = []
+        font = ImageFont.truetype("LiberationSans-Regular.ttf", args.font_size)
+        for loc_name, parts in sorted(parts_by_location.items(), key=lambda e: e[0]):
+            if len(parts) > args.max_parts_per_label:
+                print("Skipping storage location {}: {} parts".format(loc_name, len(parts)))
+                continue
+            print("Processing storage location {}: {} parts".format(loc_name, len(parts)))
             
+            img = Image.new("RGB", (label_width_px, label_height_px), 'white')
+            draw = ImageDraw.Draw(img)
+            
+            margin = round(max(label_height_px * 0.02, label_width_px * 0.02))
+            avail_label_height = label_height_px - 2 * margin
+            avail_label_width = label_width_px - 2 * margin
+            
+            # Split label into base grid with fixed height location tag and variable height parts areas
+            base_x = margin
+            loc_area_y = margin
+            loc_area_height = round(args.font_size * 1.5)
+            parts_area_y = loc_area_y + loc_area_height
+            parts_area_height = avail_label_height - loc_area_height
+            
+            # Split parts area into evenly-spaced grid
+            parts_area_region_height = parts_area_height // len(parts)
+            parts_area_regions_y = []
+            for i in range(len(parts)):
+                region_y = parts_area_y + parts_area_region_height * i
+                parts_area_regions_y.append(region_y)
+            
+            draw.text((base_x, loc_area_y), "Location: {}".format(loc_name), 'black', font=font)
+            
+            for i, part in enumerate(sorted(parts, key=lambda p: p['@id'])):
+                part_id = part['@id'].split("/")[-1]
+                barcode_height = parts_area_region_height - round(args.font_size * 1.5)
+                barcode_thickness = avail_label_width // 100
+                
+                barcode = code128.image("P" + part_id, height=barcode_height, thickness=barcode_thickness, quiet_zone=False)
+                barcode_x = (avail_label_width - (barcode.size[0])) // 2
+                barcode_y = parts_area_regions_y[i]
+                img.paste(barcode, (barcode_x, barcode_y))
+                
+                name_x = barcode_x
+                name_y = barcode_y + barcode_height + args.font_size * 0.1
+                draw.text((name_x, name_y), part['name'], 'black', font=font)
+            
+            labels.append(img)
+        
+        print("Generating PDF")
+        labels[0].save(args.label_file, "PDF", resolution=args.label_dpi, save_all=True, append_images=labels[1:])
+        
 
 if __name__ == "__main__":
     main()
